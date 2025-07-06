cae6d1a67b427c77ce2e7ac395c1c8d9c05080cc
stevending1st <stevending1st@163.com>
2025-07-07 03:53:10 +0800

    feat: add preline

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import ClientSessionProvider from "@/components/ClientProvider";
+import PrelineScriptWrapper from "@/components/PrelineScriptWrapper";
 
 import "./globals.css";
 
@@ -41,6 +42,7 @@ export default function RootLayout({
           <Footer />
         </ClientSessionProvider>
       </body>
+       <PrelineScriptWrapper />
     </html>
   );
 }
